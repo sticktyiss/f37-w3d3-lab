3313fe278d9922e382467fec6bf997a4bf489eab
Tyiss Goodman <sticktyiss@gmail.com>
2023-03-15 15:34:38 -0700

    Finished Lab

---
+++ index.js
@@ -0,0 +1,36 @@
+let message = document.querySelector('#message')
+
+const addMovie = event => {
+  event.preventDefault()
+  let inputField = document.querySelector('input')
+  let movie = document.createElement('li')
+  let movieTitle = document.createElement('span')
+  movieTitle.textContent = inputField.value
+  movieTitle.addEventListener('click', crossOffMovie)
+  movie.appendChild(movieTitle)
+  let deleteBtn = document.createElement('button')
+  deleteBtn.textContent = 'X'
+  deleteBtn.addEventListener('click', deleteMovie)
+  movie.appendChild(deleteBtn)
+  document.querySelector('ul').appendChild(movie)
+  inputField.value = ''
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+const deleteMovie = event => {
+  event.target.parentNode.remove()
+  message.textContent = `${event.target.previousSibling.textContent} deleted`
+  revealMessage()
+}
+
+const crossOffMovie = event => {
+  event.target.classList.toggle('checked')
+  event.target.classList.contains('checked') ? message.textContent = `${event.target.textContent} has been watched` : message.textContent = `${event.target.textContent} has been re-added`
+  revealMessage()
+}
+
+const revealMessage = () => {
+  message.classList.remove('hide')
+  setTimeout(() => message.classList.add('hide'), 1000)
+}
